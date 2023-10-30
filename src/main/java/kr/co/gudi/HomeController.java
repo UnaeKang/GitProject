f6d5a30a311727936493400ce5830f04238f5145
kang_unae <inpou248@gmail.com>
2023-10-30 14:36:11 +0900

    최초 프로젝트 업로드

---
+++ src/main/java/kr/co/gudi/HomeController.java
@@ -0,0 +1,24 @@
+package kr.co.gudi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		logger.info("index");
+		model.addAttribute("msg","hello git");
+
+		
+		return "home";
+	}
+	
+}
